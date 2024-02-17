9507e6444132494fe1fb8fb8f2d7792e2833fee1
maurineiko <nohaskett@gmail.com>
2024-02-16 18:55:02 -0700

    Last Check with Rubric and  Comments

--- Mission06_Haskett/Controllers/HomeController.cs
+++ Mission06_Haskett/Controllers/HomeController.cs
@@ -11,17 +11,17 @@ namespace Mission06_Haskett.Controllers
         {
             _context = temp;
         }
-        public IActionResult Index()
+        public IActionResult Index() // For the index page view and action
         {
             return View();
         }
 
-        public IActionResult KnowJoel()
+        public IActionResult KnowJoel() // For the Get to Knoow Joel Page. Just return the same file.
         {
             return View();
         }
 
-        [HttpGet]
+        [HttpGet] // Get and Post methods to submit the form and post it to the database
         public IActionResult Collection()
         {
             return View();
@@ -31,7 +31,7 @@ namespace Mission06_Haskett.Controllers
         public IActionResult Collection(MovieSurvey response)
         {
             _context.Movies.Add(response); // Add record to database
-            _context.SaveChanges();
+            _context.SaveChanges(); // save the record in the database
 
             return View("Confirmation", response);
         }
--- Mission06_Haskett/Models/MovieSurvey.cs
+++ Mission06_Haskett/Models/MovieSurvey.cs
@@ -5,8 +5,8 @@ namespace Mission06_Haskett.Models
     public class MovieSurvey
     {
         [Key]
-        [Required]
-        public int MovieEntryID { get ; set; }
+        [Required] // This parameter means that the applicaiton will break if it is not included. But I account for this as well in the actual form on the client side.
+        public int MovieEntryID { get ; set; } // Getters and setters for each table attribute
         [Required]
         public string Category { get; set;}
         public string? Category2 { get; set; }
@@ -20,8 +20,8 @@ namespace Mission06_Haskett.Models
         public string Rating { get; set; }
         public bool? Edited { get; set; }
         public string? LentTo { get; set; }
-        [MaxLength(25)]
-        [StringLength(25)]
-        public string? Notes { get; set; }
+        [MaxLength(25)] // Client Side entry validation
+        [StringLength(25)] // Server side entry validation
+        public string? Notes { get; set; } // ? parameter allows the field to be null. though a string attribute already has the option to be null
     }
 }
