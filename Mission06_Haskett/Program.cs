e3e13a37ff72ca4bd4af4b891134450a1e0f2a84
maurineiko <nohaskett@gmail.com>
2024-02-20 13:52:58 -0700

    Testing for the new branch

--- Mission06_Haskett/Program.cs
+++ Mission06_Haskett/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<MovieSurveyContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. TESTING TESTING TESTING
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
