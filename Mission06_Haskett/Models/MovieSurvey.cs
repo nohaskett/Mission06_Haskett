82fd8ede5b2f868dbe94a257ccef38211c7186ae
maurineiko <nohaskett@gmail.com>
2024-02-21 13:40:07 -0700

    Deleting Category 2

--- Mission06_Haskett/Models/MovieSurvey.cs
+++ Mission06_Haskett/Models/MovieSurvey.cs
@@ -6,11 +6,9 @@ namespace Mission06_Haskett.Models
     {
         [Key]
         [Required] // This parameter means that the applicaiton will break if it is not included. But I account for this as well in the actual form on the client side.
-        public int MovieEntryID { get ; set; } // Getters and setters for each table attribute
+        public int MovieId { get ; set; } // Getters and setters for each table attribute
         [Required]
-        public string Category { get; set;}
-        public string? Category2 { get; set; }
-        [Required]
+        public string Category { get; set; } // Need to fix this to be category ID, not category
         public string Title { get; set; }
         [Required]
         public int Year { get; set; }
@@ -20,6 +18,8 @@ namespace Mission06_Haskett.Models
         public string Rating { get; set; }
         public bool? Edited { get; set; }
         public string? LentTo { get; set; }
+        [Required]
+        public int CopiedToPlex { get; set; }
         [MaxLength(25)] // Client Side entry validation
         [StringLength(25)] // Server side entry validation
         public string? Notes { get; set; } // ? parameter allows the field to be null. though a string attribute already has the option to be null
